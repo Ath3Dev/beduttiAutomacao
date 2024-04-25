2cc5054a5fd9b6e49d80ee99d2c7eb44e7e71a43
Arthur.exe <arthur.aba20@gmail.com>
2024-04-25 01:19:43 -0300

    :sparkles: feat:  Arquivos da página Home

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import '../assets/css/Home.css'
+
+export function Home() {
+
+}
