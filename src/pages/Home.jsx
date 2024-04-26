36b4fb07908fcec8b1081a26069a5cec08ef8072
Arthur.exe <arthur.aba20@gmail.com>
2024-04-26 17:10:01 -0300

    :sparkles: feat: Adicionando o primeiro carrossel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,38 @@
-import '../assets/css/Home.css'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import image from '../assets/img/main-carousel/caroussel-images.svg';
+import image2 from '../assets/img/main-carousel/image-caroussel-2.svg';
+import image3 from '../assets/img/main-carousel/image-caroussel-3.svg';
+
+import '../assets/css/Home.css';
 
 export function Home() {
-    return(
-        <div>
-            <h1>Teste</h1>
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    };
+
+    return (
+        <div className="home-container">
+            <div className="main-carousel">
+                <Slider {...settings} className='slider'>
+                    <div>
+                        <img src={image} alt="Imagem 1" />
+                    </div>
+                    <div>
+                        <img src={image2} alt="Imagem 2" />
+                    </div>
+                    <div>
+                        <img src={image3} alt="Imagem 3" />
+                    </div>
+                </Slider>
+            </div>
         </div>
-    )
+    );
 }
